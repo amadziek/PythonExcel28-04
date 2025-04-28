6def4cec6a9bf5f431482304d5ec1271f4867a1b
amadziek <pyda.magdalena@gmail.com>
2025-04-28 14:54:51 +0200

    [1 6] operacje na pliku excel

--- ex3.py
+++ ex3.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import openpyxl
 
 wb = openpyxl.load_workbook('./data/videogamesales.xlsx')
@@ -9,8 +11,28 @@ ws = wb['vgsales']
 print("total number of ros: " + str(ws.max_row))  # total number of ros: 16328
 print("total number of columns" + str(ws.max_column))  # total number of columns10
 
-print("Vaule in cell A1 is: " , ws['A1'].value) # Vaule in cell A1 is:  Rank
+print("Vaule in cell A1 is: ", ws['A1'].value)  # Vaule in cell A1 is:  Rank
 
-values = [ws.cell(row=1, column=i).value for i in range(1, ws.max_column)]
-print(values)  # ['Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales', 'EU_Sales', 'JP_Sales']
+# values = [ws.cell(row=1, column=i).value for i in range(1, ws.max_column)]
+# print(values)  # ['Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales', 'EU_Sales', 'JP_Sales']
 
+# data = [ws.cell(row=i, column=2).value for i in range(2, 12)]  # ['Wii Sports', 'Super Mario Bros.', 'Mario Kart Wii', 'Wii Sports Resort', 'Pokemon Red/Pokemon Blue', 'Tetris', 'New Super Mario Bros.', 'Wii Play', 'New Super Mario Bros. Wii', 'Duck Hunt']
+# print(data)
+
+# pprint(data)
+
+my_list = list()  # pusta lista
+
+for value in ws.iter_rows(
+        min_row=1, max_row=11, min_col=1, max_col=6,
+        values_only=True):
+    my_list.append(value)
+print(my_list)
+
+for ele1, ele2, ele3, ele4, ele5, ele6 in my_list:
+    print(f"{ele1:<8}{ele2:>35}{ele3:^10}{ele4:<10}{ele5:<15}{ele6:<15}")
+
+ws["K1"] = "Sum of Sales"
+
+wb.save('videogamesales2.xlsx')
+wb.close()
