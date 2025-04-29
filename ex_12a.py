f531eccf11db0b5c4177e5d8bd77315c6f451717
amadziek <pyda.magdalena@gmail.com>
2025-04-29 11:27:53 +0200

    [2 4] pandas i abele z filtrami

---
+++ ex_12a.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = [
+    ["Aditya", 179],
+    ['Sameer', 181],
+    ['darwin', 170],
+    ['Joel', 167]
+]
+
+column_names = ['Name', 'Hight']
+
+df = pd.DataFrame(data, columns=column_names)
+writer = pd.ExcelWriter('excel_with_list.xlsx', engine='xlsxwriter')
+
+df.to_excel(writer, sheet_name='first_sheet', index=False, startrow=3, startcol=4)
+writer.close()
