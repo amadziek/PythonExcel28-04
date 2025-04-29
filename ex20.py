6632877031667f8820b9d1acd5ddc2349de1f722
amadziek <pyda.magdalena@gmail.com>
2025-04-29 13:10:32 +0200

    [2 8] pandas cd

--- ex20.py
+++ ex20.py
@@ -3,5 +3,9 @@ import pandas as pd
 df = pd.read_excel('excel_with_multiple_sheets-1.xlsx')
 print(df)
 
-suma = df['Height'].sum(skipna=False) # omin barkujace
+print(df[df["Height"] > 175 ])
+suma = df['Height'].sum(skipna=False) # omiń barkujace
 print("Suma:", suma)
+
+print(df.info)
+print(df.describe)
